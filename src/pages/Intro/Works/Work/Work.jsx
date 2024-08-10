50902800ea0b99f905280beae08dc36915342349
max200pl <max2000pl@gmail.com>
2024-08-10 18:48:11 +0300

    #added header and edit button for dev mode

--- src/pages/Intro/Works/Work/Work.jsx
+++ src/pages/Intro/Works/Work/Work.jsx
@@ -1,54 +1,69 @@
 import s from "./Work.module.scss";
 import {
-  getFolderName,
-  getImageName,
-  getYear,
+    getFolderName,
+    getImageName,
+    getYear,
 } from "../../../../assets/helpers/helpers";
 import ImageLazyLoad from "../../../../assets/components/ImageLazyLoad/ImageLazyLoad";
 import { defUrlWorkImage } from "../../../../assets/api/constants";
+import editIcon from "../../../../assets/images/intro/edit.svg";
+import { Fade } from "react-awesome-reveal";
 
 export const Work = ({
-  category,
-  date,
-  name,
-  cardImage,
-  onCardClick,
-  onClickEditWork,
+    category,
+    date,
+    name,
+    cardImage,
+    onCardClick,
+    onClickEditWork,
+    editSection,
 }) => {
-  const imageName = getImageName(cardImage.name);
-  const folderName = getFolderName(cardImage.name);
-  const urlImage = defUrlWorkImage(folderName, imageName);
+    const imageName = getImageName(cardImage.name);
+    const folderName = getFolderName(cardImage.name);
+    const urlImage = defUrlWorkImage(folderName, imageName);
 
-  return (
-    <div className={s.work} onClick={() => onCardClick()}>
-      <button
-        className={s.work__edit_button}
-        onClick={(e) => {
-          e.stopPropagation();
-          onClickEditWork();
-        }}
-      >
-        Edit work
-      </button>
+    return (
+        <div className={s.work} onClick={() => onCardClick()}>
+            <div className={s.work__container}>
+                {editSection && (
+                    <Fade
+                        className={s.work__container_edit_button}
+                        triggerOnce="true"
+                        direction="right"
+                    >
+                        <button
+                            className={s.work__edit_button}
+                            onClick={(e) => {
+                                e.stopPropagation();
+                                onClickEditWork();
+                            }}
+                        >
+                            <img
+                                className={s.button__image}
+                                src={editIcon}
+                                alt="Close"
+                            />
+                        </button>
+                    </Fade>
+                )}
 
-      <div className={s.work__container}>
-        <div className={s.work__image}>
-          <ImageLazyLoad
-            mixin="work"
-            blurHash={cardImage.blurHash}
-            name={cardImage.name}
-            url={urlImage}
-          />
+                <div className={s.work__image}>
+                    <ImageLazyLoad
+                        mixin="work"
+                        blurHash={cardImage.blurHash}
+                        name={cardImage.name}
+                        url={urlImage}
+                    />
+                </div>
+
+                <div className={s.work__content}>
+                    <div className={s.work__cat}>{category}</div>
+                    <div className={s.work__title}>
+                        {name}
+                        <time className={s.work__date}>{getYear(date)}</time>
+                    </div>
+                </div>
+            </div>
         </div>
-
-        <div className={s.work__content}>
-          <div className={s.work__cat}>{category}</div>
-          <div className={s.work__title}>
-            {name}
-            <time className={s.work__date}>{getYear(date)}</time>
-          </div>
-        </div>
-      </div>
-    </div>
-  );
+    );
 };
--- src/pages/Intro/Works/Works.jsx
+++ src/pages/Intro/Works/Works.jsx
@@ -8,28 +8,68 @@ import ModalWork from "../../../modals/ModalWork/ModalWork";
 import Modal from "../../../assets/components/Modal/Modal";
 import { LazyLoadComponent } from "react-lazy-load-image-component";
 import ModalWorkManager from "../../../modals/ModalWorkManager/ModalWorkManager";
-import WorkAdd from "./Work/WorkAdd/WorkAdd";
-import { useGetCategoriesQuery, useGetWorksQuery } from "../../../assets/api/works.api";
+import {
+    useGetCategoriesQuery,
+    useGetWorksQuery,
+} from "../../../assets/api/works.api";
+
+import editIcon from "../../../assets/images/intro/edit.svg";
+
+import { ReactComponent as PlusImage } from "../../../assets/images/plus.svg";
 
 const Works = () => {
     const [filter, setFilter] = useState({ category: "" });
     const { status, data: works } = useGetWorksQuery(filter);
 
-    const { status: statusCategories, data: categories } = useGetCategoriesQuery();
+    const { status: statusCategories, data: categories } =
+        useGetCategoriesQuery();
     const uniqCategoriesWork = getUniqCategoriesWork(categories);
 
     const [isOpenModal, toggleOpenModal] = useState(false);
     const [isOpenEditWorkModal, toggleEditWorkOpenModal] = useState(false);
     const [currentWork, setCurrentWork] = useState({});
+    const [editSection, setEditSection] = useState(false);
 
     return (
         <div className={s.portfolio} id="portfolio">
-            {statusCategories === "success" && (
-                <FilterWorks
-                    onFilterChange={setFilter}
-                    categories={uniqCategoriesWork}
-                />
-            )}
+            <div className={s.portfolio__header}>
+                <div className={s.portfolio__header_filter}>
+                    {statusCategories === "success" && (
+                        <FilterWorks
+                            onFilterChange={setFilter}
+                            categories={uniqCategoriesWork}
+                        />
+                    )}
+                </div>
+
+                <div className={s.portfolio__header_action_panel}>
+                    {editSection && (
+                        <Fade triggerOnce="true" direction="right">
+                            <button
+                                onClick={() => {
+                                    setCurrentWork(undefined);
+                                    toggleEditWorkOpenModal(true);
+                                }}
+                                type="button"
+                            >
+                                <PlusImage className={s.work_add__image_plus} />
+                            </button>
+                        </Fade>
+                    )}
+
+                    <button
+                        className={s.portfolio__header_edit_button}
+                        onClick={() => setEditSection(!editSection)}
+                        type="button"
+                    >
+                        <img
+                            className={s.button__image}
+                            src={editIcon}
+                            alt="Close"
+                        />
+                    </button>
+                </div>
+            </div>
 
             {status === "success" && (
                 <div className={s.portfolio__container}>
@@ -40,33 +80,22 @@ const Works = () => {
                             cascade
                             className={s.portfolio__col}
                         >
-                            <div className={s.portfolio__col}>
-                                <LazyLoadComponent>
-                                    <WorkAdd
-                                        onCardClick={() => {
-                                            setCurrentWork(undefined);
-                                            toggleEditWorkOpenModal(true);
-                                        }}
-                                    />
-                                </LazyLoadComponent>
-                            </div>
-                        </Fade>
-                        <Fade
-                            triggerOnce="true"
-                            direction="right"
-                            cascade
-                            className={s.portfolio__col}
-                        >
                             {works?.map((work, id) => {
                                 return (
-                                    <div key={work.name} className={s.portfolio__col}>
+                                    <div
+                                        key={work.name}
+                                        className={s.portfolio__col}
+                                    >
                                         <LazyLoadComponent>
                                             <WorkComponent
                                                 {...work}
                                                 key={work.name}
+                                                editSection={editSection}
                                                 onClickEditWork={() => {
-                                                    setCurrentWork(work)
-                                                    toggleEditWorkOpenModal(true);
+                                                    setCurrentWork(work);
+                                                    toggleEditWorkOpenModal(
+                                                        true
+                                                    );
                                                 }}
                                                 onCardClick={() => {
                                                     setCurrentWork(work);
@@ -82,7 +111,10 @@ const Works = () => {
                 </div>
             )}
 
-            <Modal handleClose={() => toggleOpenModal(false)} isOpen={isOpenModal}>
+            <Modal
+                handleClose={() => toggleOpenModal(false)}
+                isOpen={isOpenModal}
+            >
                 <ModalWork onClose={toggleOpenModal} work={currentWork} />
             </Modal>
 
