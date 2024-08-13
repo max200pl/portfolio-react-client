ae5e3243a1e2c57a722f5e48031f0b7595af5ea6
max200pl <max2000pl@gmail.com>
2024-08-13 09:19:57 +0300

    #fixed indents

--- src/index.tsx
+++ src/index.tsx
@@ -1,37 +1,35 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.scss';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
-import '@fontsource/roboto/300.css';
-import '@fontsource/roboto/400.css';
-import '@fontsource/roboto/500.css';
-import '@fontsource/roboto/700.css';
-import { CssBaseline, ThemeProvider, createTheme } from '@mui/material';
-import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
-import { LocalizationProvider } from '@mui/x-date-pickers';
-import { GoogleOAuthProvider } from '@react-oauth/google';
-import UserContextProvider from './context/user-context';
-
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.scss";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+import "@fontsource/roboto/300.css";
+import "@fontsource/roboto/400.css";
+import "@fontsource/roboto/500.css";
+import "@fontsource/roboto/700.css";
+import { CssBaseline, ThemeProvider, createTheme } from "@mui/material";
+import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
+import { LocalizationProvider } from "@mui/x-date-pickers";
+import { GoogleOAuthProvider } from "@react-oauth/google";
+import UserContextProvider from "./context/user-context";
 
 const root = ReactDOM.createRoot(
-    document.getElementById('root') as HTMLElement
+    document.getElementById("root") as HTMLElement
 );
 
 const darkTheme = createTheme({
     palette: {
-        mode: 'dark',
+        mode: "dark",
     },
 });
 
-
-const queryClient = new QueryClient()
+const queryClient = new QueryClient();
 
 root.render(
     <UserContextProvider>
-        <GoogleOAuthProvider clientId='41245498308-44oaelsqg4sgj011hevmr3953nqsmjrn.apps.googleusercontent.com'>
+        <GoogleOAuthProvider clientId="41245498308-44oaelsqg4sgj011hevmr3953nqsmjrn.apps.googleusercontent.com">
             <ThemeProvider theme={darkTheme}>
                 <LocalizationProvider dateAdapter={AdapterDayjs}>
                     <QueryClientProvider client={queryClient}>
