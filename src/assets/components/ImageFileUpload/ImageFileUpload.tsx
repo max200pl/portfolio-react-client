1f596eb4e99d2bd93d7ccad3fd18d80755c72806
max200pl <max2000pl@gmail.com>
2024-11-10 13:30:16 +0200

    #fixed styles

--- src/assets/components/ImageFileUpload/ImageFileUpload.tsx
+++ src/assets/components/ImageFileUpload/ImageFileUpload.tsx
@@ -81,7 +81,7 @@ const ImageFileUpload = <T extends FieldValues>({
     return (
         <div
             error-state={String(!!errors?.image)}
-            className={`${s.container} ${!preview ? s.empty : ""}`}
+            className={`${s.container} ${preview ? s.notEmpty : ""}`}
             {...getRootProps()}
         >
             <input
