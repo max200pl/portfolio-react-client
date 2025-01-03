e5d48928f7bca6d13bcace5c5d854a5dff58eb3f
max200pl <max2000pl@gmail.com>
2025-01-03 15:07:47 +0200

    fix: update Filter component to use Category interface and improve filter logic

--- src/assets/components/Filter/Filter.tsx
+++ src/assets/components/Filter/Filter.tsx
@@ -1,26 +1,27 @@
+import { Category } from "../../interfaces/NewInterfaces";
 import s from "./Filter.module.scss";
 
 interface FilterProps {
-    onFilterChange: (filter: { category: string }) => void;
-    categories: string[];
+    onFilterChange: (filter: Category["_id"] | undefined) => void;
+    categories: Category[] | undefined;
 }
 
 export const Filter = ({ onFilterChange, categories }: FilterProps) => {
     return (
         <div className={s.filter}>
             <button
-                onClick={() => onFilterChange({ category: "" })}
+                onClick={() => onFilterChange(undefined)}
                 className={s.filter__button}
             >
                 All
             </button>
-            {categories.map((category, id) => (
+            {categories?.map((category, id) => (
                 <button
                     key={id}
-                    onClick={() => onFilterChange({ category })}
+                    onClick={() => onFilterChange(category._id)}
                     className={s.filter__button}
                 >
-                    {category}
+                    {category.label}
                 </button>
             ))}
         </div>
