dcfc03d062116f7d21eb5e3285dc676e13f890a5
max200pl <max2000pl@gmail.com>
2025-01-03 15:08:21 +0200

    fix: migrate Works.helpers from JavaScript to TypeScript and enhance category extraction logic

---
+++ src/pages/Works/Works.helpers.ts
@@ -0,0 +1,44 @@
+import { defUrlWorkImage } from "../../assets/api/constants";
+import { getFolderName, getImageName } from "../../assets/helpers/helpers";
+import { Category, IWork } from "../../assets/interfaces/NewInterfaces";
+
+export const getUrlWorkImage = (image: any) => {
+    const folderName = getFolderName(image);
+    const imageName = getImageName(image);
+
+    return defUrlWorkImage(folderName, imageName);
+};
+
+export const extractUniqueCategories = (
+    categories: Category[] | undefined,
+    works: IWork[] | undefined
+) => {
+    const uniqueCategories = categories?.filter((category) =>
+        works?.some((work) => work.category._id === category._id)
+    );
+    return uniqueCategories;
+};
+
+export const extractUniqueCategoryLabels = (
+    categories: Category[] | undefined,
+    uniqueCategoryIds: Category["_id"][]
+) => {
+    return categories?.filter((category) =>
+        uniqueCategoryIds.includes(category._id)
+    );
+};
+
+export const getCurrentWork = (works: any[], currentWork: { name: any }) => {
+    return works.filter(
+        (work: { name: any }) => work.name === currentWork.name
+    );
+};
+
+export const handleTechUpdate = (
+    tech: { [s: string]: unknown } | ArrayLike<unknown>
+) => {
+    return Object.entries(tech).reduce((acc, [key, value]) => {
+        // acc[key] = value;
+        return acc;
+    }, {});
+};
