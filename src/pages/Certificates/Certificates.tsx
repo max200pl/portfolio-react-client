fb9123b31e4c775e8627af2b5dcbc6d82a719b82
max200pl <max2000pl@gmail.com>
2025-01-23 09:38:20 +0200

    #Certificates  #enhance layout and styling of Certificates component for improved responsiveness and user experience

--- src/pages/Certificates/Certificates.tsx
+++ src/pages/Certificates/Certificates.tsx
@@ -17,6 +17,7 @@ import ModalCertificateManager from "../../modals/ModalCertificateManager/ModalC
 import Filter from "../../assets/components/Filter/Filter";
 import { Category, ICertificate } from "../../assets/interfaces/NewInterfaces";
 import { getUniqCategoriesCertificates } from "./Certificates.helpers";
+import { Box, Grid, Skeleton } from "@mui/material";
 
 const Certificates = () => {
     const [isOpenHireMeModal, setIsOpenHireMeModal] = useState(false);
@@ -58,7 +59,57 @@ const Certificates = () => {
             <div className="container">
                 <SectionTitle text="Certifications" />
 
-                {isLoading && <Loader />}
+                {!certificates && (
+                    <>
+                        <Grid
+                            sx={{
+                                markerEnd: "15px",
+                            }}
+                            container
+                            wrap="wrap"
+                            boxSizing={"border-box"}
+                        >
+                            {new Array(6).fill(0).map((_, index) => (
+                                <Box
+                                    key={index}
+                                    flexBasis={[
+                                        "100%",
+                                        "100%",
+                                        "33.333%",
+                                        "33.333%",
+                                    ]}
+                                    sx={{
+                                        my: 0,
+                                    }}
+                                >
+                                    <Box
+                                        display={"flex"}
+                                        flexDirection={"column"}
+                                        justifyContent={"space-between"}
+                                        width={[
+                                            "100%",
+                                            "370px",
+                                            "33.333%",
+                                            "33.333%",
+                                        ]}
+                                        sx={{
+                                            margin: [
+                                                "15px auto",
+                                                "15px auto",
+                                                "15px",
+                                                "15px",
+                                            ],
+                                            my: 0,
+                                        }}
+                                    >
+                                        <Skeleton height="330px" width="100%" />
+                                        <Skeleton height="60px" width="60%" />
+                                    </Box>
+                                </Box>
+                            ))}
+                        </Grid>
+                    </>
+                )}
 
                 <ActionPanel
                     getStatusEditSection={(status) => setEditSection(status)}
@@ -77,40 +128,38 @@ const Certificates = () => {
                 </ActionPanel>
 
                 {status === "success" && certificates.length ? (
-                    <div className={s.card}>
-                        <div className={s.card__container}>
-                            <Fade
-                                triggerOnce={true}
-                                direction="right"
-                                cascade
-                                className={s.card__col}
-                            >
-                                {certificates?.map((certificate, id) => {
-                                    return (
-                                        <div
-                                            key={certificate.name}
-                                            className={s.card__col}
-                                        >
-                                            <LazyLoadComponent>
-                                                <Certificate
-                                                    certificate={certificate}
-                                                    key={certificate._id}
-                                                    editSection={editSection}
-                                                    onClickEditCertificate={() => {
-                                                        setCurrentCertificate(
-                                                            certificate
-                                                        );
-                                                        toggleEditOpenModal(
-                                                            true
-                                                        );
-                                                    }}
-                                                />
-                                            </LazyLoadComponent>
-                                        </div>
-                                    );
-                                })}
-                            </Fade>
-                        </div>
+                    <div className={s.certificates_container}>
+                        <Fade
+                            triggerOnce={true}
+                            direction="right"
+                            cascade
+                            className={s.certificates_container__col}
+                        >
+                            {certificates?.map((certificate, id) => {
+                                return (
+                                    <div
+                                        key={certificate.name}
+                                        className={
+                                            s.certificates_container__col
+                                        }
+                                    >
+                                        <LazyLoadComponent>
+                                            <Certificate
+                                                certificate={certificate}
+                                                key={certificate._id}
+                                                editSection={editSection}
+                                                onClickEditCertificate={() => {
+                                                    setCurrentCertificate(
+                                                        certificate
+                                                    );
+                                                    toggleEditOpenModal(true);
+                                                }}
+                                            />
+                                        </LazyLoadComponent>
+                                    </div>
+                                );
+                            })}
+                        </Fade>
                     </div>
                 ) : null}
 
