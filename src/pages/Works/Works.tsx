108101a58b7b457b3c031f81021bfb6ff8013f68
max200pl <max2000pl@gmail.com>
2025-01-23 09:37:58 +0200

    #Works #enhance layout and styling of Works component for improved responsiveness and user experience

--- src/pages/Works/Works.tsx
+++ src/pages/Works/Works.tsx
@@ -4,15 +4,11 @@ import Modal from "../../assets/components/Modal/Modal";
 import ModalHireMe from "../../modals/ModalHireMe/ModalHireMe";
 import ModalSeeMyResume from "../../modals/ModalSeeMyResume/ModalSeeMyResume";
 import { Fade } from "react-awesome-reveal";
-import {
-    useGetCategoriesWorksQuery,
-    useGetWorksQuery,
-} from "../../assets/api/works.api";
+import { useGetWorksQuery } from "../../assets/api/works.api";
 import ModalWork from "../../modals/ModalWork/ModalWork";
 import ModalWorkManager from "../../modals/ModalWorkManager/ModalWorkManager";
 import SectionTitle from "../../assets/components/SectionTitle/SectionTitle";
 import ActionPanel from "../../assets/components/ActionPanel/ActionPanel";
-import Loader from "../../assets/components/Loader/Loader";
 import Filter from "../../assets/components/Filter/Filter";
 import { Category, IWork } from "../../assets/interfaces/NewInterfaces";
 import { Work } from "./Work/Work";
@@ -64,19 +60,24 @@ const Works = () => {
 
                 {!works && (
                     <>
-                        <Loader />
-                        <Grid container wrap="wrap" boxSizing={"border-box"}>
+                        <Grid
+                            sx={{
+                                markerEnd: "15px",
+                            }}
+                            container
+                            wrap="wrap"
+                            boxSizing={"border-box"}
+                        >
                             {new Array(6).fill(0).map((_, index) => (
                                 <Box
                                     key={index}
                                     flexBasis={[
                                         "100%",
-                                        "50%",
+                                        "100%",
                                         "33.333%",
                                         "33.333%",
                                     ]}
                                     sx={{
-                                        // margin: "15px",
                                         my: 0,
                                     }}
                                 >
@@ -84,14 +85,24 @@ const Works = () => {
                                         display={"flex"}
                                         flexDirection={"column"}
                                         justifyContent={"space-between"}
-                                        flexBasis={["100%"]}
+                                        width={[
+                                            "100%",
+                                            "370px",
+                                            "33.333%",
+                                            "33.333%",
+                                        ]}
                                         sx={{
-                                            margin: "15px",
+                                            margin: [
+                                                "15px auto",
+                                                "15px auto",
+                                                "15px",
+                                                "15px",
+                                            ],
                                             my: 0,
                                         }}
                                     >
-                                        <Skeleton height="250px" width="100%" />
-                                        <Skeleton width="60%" />
+                                        <Skeleton height="330px" width="100%" />
+                                        <Skeleton height="60px" width="60%" />
                                     </Box>
                                 </Box>
                             ))}
@@ -99,42 +110,38 @@ const Works = () => {
                     </>
                 )}
                 {status === "success" && (
-                    <div className={s.card}>
-                        <div className={s.card__container}>
-                            <Fade
-                                triggerOnce={true}
-                                direction="right"
-                                cascade
-                                className={s.card__col}
-                            >
-                                {works?.map((work, id) => {
-                                    return (
-                                        <div
-                                            key={work.name}
-                                            className={s.card__col}
-                                        >
-                                            <LazyLoadComponent>
-                                                <Work
-                                                    work={work}
-                                                    key={String(work._id)}
-                                                    editSection={editSection}
-                                                    onClickEditWork={() => {
-                                                        setCurrentWork(work);
-                                                        toggleEditOpenModal(
-                                                            true
-                                                        );
-                                                    }}
-                                                    onCardClick={() => {
-                                                        setCurrentWork(work);
-                                                        toggleOpenModal(true);
-                                                    }}
-                                                />
-                                            </LazyLoadComponent>
-                                        </div>
-                                    );
-                                })}
-                            </Fade>
-                        </div>
+                    <div className={s.works_container}>
+                        <Fade
+                            triggerOnce={true}
+                            direction="right"
+                            cascade
+                            className={s.works_container__col}
+                        >
+                            {works?.map((work, id) => {
+                                return (
+                                    <div
+                                        key={work.name}
+                                        className={s.works_container__col}
+                                    >
+                                        <LazyLoadComponent>
+                                            <Work
+                                                work={work}
+                                                key={String(work._id)}
+                                                editSection={editSection}
+                                                onClickEditWork={() => {
+                                                    setCurrentWork(work);
+                                                    toggleEditOpenModal(true);
+                                                }}
+                                                onCardClick={() => {
+                                                    setCurrentWork(work);
+                                                    toggleOpenModal(true);
+                                                }}
+                                            />
+                                        </LazyLoadComponent>
+                                    </div>
+                                );
+                            })}
+                        </Fade>
                     </div>
                 )}
 
