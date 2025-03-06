49a9b7ff6799ce24bdea673d36fb82aecc4d8684
max200pl <max2000pl@gmail.com>
2025-03-06 12:00:37 +0200

    feat: add Google sign-in button component with error handling and logging

--- src/assets/components/ButtonAuthGoogle/ButtonAuthGoogle.tsx
+++ src/assets/components/ButtonAuthGoogle/ButtonAuthGoogle.tsx
@@ -1,29 +1,29 @@
 import { useContext, useState } from "react";
 import { useNavigate } from "react-router-dom";
-import { GithubLoginButton } from "react-social-login-buttons";
+import { GoogleLoginButton } from "react-social-login-buttons";
 import { TypeActionAuth } from "../../api/auth.api";
-import s from "./ButtonAuthGitHub.module.scss";
+import s from "./ButtonAuthGoogle.module.scss";
 import { ErrorMessage } from "../../../forms/AuthForm/ErrorMessage";
 import { AuthContext } from "../../../context/auth-context";
 import { formatFirebaseErrorMessages } from "../../../forms/forms.helpers";
 import { logError } from "../../../utils/loggingHelpers";
 
-const ButtonAuthGitHub = ({ typeAction }: { typeAction: TypeActionAuth }) => {
+const ButtonAuthGoogle = ({ typeAction }: { typeAction: TypeActionAuth }) => {
     const authCtx = useContext(AuthContext);
     const navigate = useNavigate();
     const [showError, setError] = useState<{ message: string }>();
 
-    const githubLoginHandler = async () => {
+    const googleLoginHandler = async () => {
         try {
-            await authCtx.signInWithGitHub();
+            await authCtx.signInWithGoogle();
             navigate("/");
         } catch (error) {
             const errorCode = (error as { code: string }).code;
             const errorMessage = formatFirebaseErrorMessages(
                 errorCode,
-                "github"
+                "google"
             );
-            logError("Error logging in with GitHub:", errorMessage);
+            logError("Error logging in with Google:", errorMessage);
             setError({ message: errorMessage });
         }
     };
@@ -31,14 +31,14 @@ const ButtonAuthGitHub = ({ typeAction }: { typeAction: TypeActionAuth }) => {
     return (
         <>
             {showError && <ErrorMessage message={showError.message} />}
-            <GithubLoginButton
-                text="GitHub"
+            <GoogleLoginButton
+                text="Google"
                 className={`${s["form_control"]} ${s["form_control__login"]}`}
-                onClick={githubLoginHandler}
+                onClick={googleLoginHandler}
                 align="center"
             />
         </>
     );
 };
 
-export default ButtonAuthGitHub;
+export default ButtonAuthGoogle;
