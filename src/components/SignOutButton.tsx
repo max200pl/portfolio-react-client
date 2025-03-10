fa2700c353c613d80845fde29766e8f51db26c27
max200pl <max2000pl@gmail.com>
2025-03-10 15:42:23 +0200

    feat: add navigation to home page after successful sign-out in SignOutButton component

--- src/components/SignOutButton.tsx
+++ src/components/SignOutButton.tsx
@@ -1,13 +1,16 @@
 import React, { useContext } from "react";
 import { AuthContext } from "../context/auth-context";
+import { useNavigate } from "react-router-dom";
 
 const SignOutButton: React.FC = () => {
+    const navigate = useNavigate();
     const { signOut } = useContext(AuthContext);
 
     const handleSignOut = async () => {
         try {
             await signOut();
             alert("Successfully signed out.");
+            navigate("/");
         } catch (error) {
             console.error("Error signing out:", error);
             alert("Error signing out. Please try again.");
