6a558dded3545ac61b342b2f1f767061c7979611
Maksym Poskannyi <max2000pl@gmail.com>
2026-03-11 16:24:38 +0200

    feat: update CV download link in DownloadCVButton component

--- src/components/DownloadCVButton/DownloadCVButton.tsx
+++ src/components/DownloadCVButton/DownloadCVButton.tsx
@@ -4,7 +4,7 @@ const DownloadCVButton = () => {
             className={"btn"}
             onClick={() => {
                 window.open(
-                    "https://drive.google.com/file/d/1Kb5FRdToChpIsSvoVx1ACAMBvLz05Jw4/view?usp=sharing",
+                    "https://www.canva.com/design/DAHCgtC_ySs/IOr8Hbyc66-9bc4n0L2YCg/view?utm_content=DAHCgtC_ySs&utm_campaign=designshare&utm_medium=link2&utm_source=uniquelinks&utlId=h71c46c78c9",
                     "_blank"
                 );
             }}
